6593eb2a9378c06fa0bce25678ebd16837287f6e
pipopotamasu <ec0156hx@gmail.com>
2021-04-04 16:02:45 +0900

    Skip header (#6)
    * skip header
    
    * refactor

--- src/words_counter/counter.rs
+++ src/words_counter/counter.rs
@@ -2,8 +2,13 @@ use std::io::BufRead;
 use anyhow::Result;
 use std::collections::HashMap;
 
-pub fn run<R: BufRead>(reader: R) -> Result<()> {
+pub fn run<R: BufRead>(mut reader: R, skip_header: bool) -> Result<()> {
   let mut counter: HashMap<String, u32> = HashMap::new();
+
+  if skip_header {
+    let mut buf = String::new();
+    reader.read_line(&mut buf).unwrap_err();
+  }
 
   for line in reader.lines() {
       let l = line?;
